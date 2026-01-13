bd9f3bf53c9c8da85ed149fc2af3253a8f990506
dylanluo05 <dylan.luo.777@gmail.com>
2026-01-13 01:35:36 -0500

    Initial Commit

---
+++ Fitspo/FitspoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitspoApp.swift
+//  Fitspo
+//
+//  Created by Dylan Luo on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct FitspoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitspoUITests/FitspoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FitspoUITestsLaunchTests.swift
+//  FitspoUITests
+//
+//  Created by Dylan Luo on 1/13/26.
+//
+
+import XCTest
+
+final class FitspoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
